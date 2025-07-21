6f8c9262c29385451fad4a72fbe3b5f0174c350b
Muhammed Reda <moreda263@gmail.com>
2025-07-21 05:31:01 +0300

    add resourse filter

---
+++ CRUDExample/Filters/ResourseFilters/FeatureDisabledResourceFilter.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace CRUDExample.Filters.ResourseFilters
+{
+    public class FeatureDisabledResourceFilter : IAsyncResourceFilter
+    {
+        private readonly ILogger<FeatureDisabledResourceFilter> _logger;
+        bool isDisable;
+
+        public FeatureDisabledResourceFilter(ILogger<FeatureDisabledResourceFilter> logger, bool _isDisable = true)
+        {
+            _logger = logger;
+            isDisable = _isDisable;
+        }
+        public async Task OnResourceExecutionAsync(ResourceExecutingContext context, ResourceExecutionDelegate next)
+        {
+            _logger.LogInformation("{FilterName}.{MethodName} - before", nameof(FeatureDisabledResourceFilter), nameof(OnResourceExecutionAsync));
+            if (isDisable)
+            {
+                context.Result = new StatusCodeResult(501);
+            }
+            else { 
+            await next();
+            }
+
+            _logger.LogInformation("{FilterName}.{MethodName} - after", nameof(FeatureDisabledResourceFilter), nameof(OnResourceExecutionAsync));
+        }
+    }
+}
